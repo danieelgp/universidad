9d01902ef15f6b632970ffdc5b9be05418be9f82
danieelgp <164122759+danieelgp@users.noreply.github.com>
2025-09-03 01:10:11 +0000

    Actualización de settings, urls y templates

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("estudiantes.urls", namespace="estudiantes")),  # 🔹 tu app
+    path("", include("estudiantes.urls", namespace="estudiantes")), 
 ]
